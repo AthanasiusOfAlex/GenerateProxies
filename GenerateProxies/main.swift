7be9f3ecba4cb95149d21511873c65aae1094a47
AthanasiusOfAlex <lmelahn@Louiss-MacBook-Pro.local>
2016-11-14 11:50:08 +0100

    Initial Commit

---
+++ GenerateProxies/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenerateProxies
+//
+//  Created by Louis Melahn on 11/14/16.
+//  Copyright © 2016 Louis Melahn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
